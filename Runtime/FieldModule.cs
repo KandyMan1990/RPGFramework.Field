9ad43af7f0422a1c93e93005f76ba0a413b5c2f0
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-01-18 21:43:59 +0000

    FieldModule can now load MenuModule

--- Runtime/FieldModule.cs
+++ Runtime/FieldModule.cs
@@ -1,21 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using RPGFramework.Core;
 using RPGFramework.Core.Input;
 using RPGFramework.Core.SharedTypes;
 using RPGFramework.DI;
 using RPGFramework.Field.SharedTypes;
-using UnityEngine;
+using RPGFramework.Menu.SharedTypes;
+using Object = UnityEngine.Object;
 
 namespace RPGFramework.Field
 {
-    public class FieldModule : IFieldModule
+    public class FieldModule : IFieldModule, IInputContext
     {
-        private readonly IDIResolver m_DIResolver;
+        private readonly ICoreModule       m_CoreModule;
+        private readonly IDIResolver       m_DIResolver;
+        private readonly IInputRouter      m_InputRouter;
+        private readonly IMenuTypeProvider m_MenuTypeProvider;
 
         private InputAdapter m_InputAdapter;
 
-        public FieldModule(IDIResolver diResolver)
+        public FieldModule(ICoreModule       coreModule,
+                           IDIResolver       diResolver,
+                           IInputRouter      inputRouter,
+                           IMenuTypeProvider menuTypeProvider)
         {
-            m_DIResolver = diResolver;
+            m_CoreModule       = coreModule;
+            m_DIResolver       = diResolver;
+            m_InputRouter      = inputRouter;
+            m_MenuTypeProvider = menuTypeProvider;
         }
 
         Task IModule.OnEnterAsync(IModuleArgs args)
@@ -24,14 +36,33 @@ namespace RPGFramework.Field
             m_DIResolver.InjectInto(m_InputAdapter);
             m_InputAdapter.Enable();
 
+            m_InputRouter.Push(this);
+
             return Task.CompletedTask;
         }
 
         Task IModule.OnExitAsync()
         {
+            m_InputRouter.Pop(this);
+
             m_InputAdapter.Disable();
 
+            m_CoreModule.ResetModule<IFieldModule, FieldModule>();
+
             return Task.CompletedTask;
+        }
+
+        bool IInputContext.Handle(ControlSlot slot)
+        {
+            if (slot == ControlSlot.Tertiary)
+            {
+                Type            type = m_MenuTypeProvider.GetType(MenuType.Config);
+                IMenuModuleArgs args = new MenuModuleArgs(type);
+
+                m_CoreModule.LoadModuleAsync<IMenuModule>(args).FireAndForget();
+            }
+
+            return true;
         }
     }
 }
