20ec7772c802d087e1121abae2f0df2a515eb066
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-02-12 23:00:13 +0000

    Add Interaction trigger

---
+++ Runtime/FieldInteractionTrigger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPGFramework.Field
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public sealed class FieldInteractionTrigger : MonoBehaviour
+    {
+        public event Action<int, int> OnInteracted;
+        public event Action<int>      OnTriggerEntered;
+        public event Action<int>      OnTriggerExited;
+
+        private FieldEntity m_Entity;
+        private bool        m_IsActive;
+        private int         m_EntityId;
+
+        private void Awake()
+        {
+            m_Entity   = GetComponentInParent<FieldEntity>();
+            m_IsActive = true;
+            m_EntityId = m_Entity.EntityId;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TriggerEnterLogic();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            TriggerEnterLogic();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            TriggerExitLogic();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            TriggerExitLogic();
+        }
+
+        private void TriggerEnterLogic()
+        {
+            if (!m_IsActive)
+            {
+                return;
+            }
+
+            OnTriggerEntered?.Invoke(m_EntityId);
+        }
+
+        private void TriggerExitLogic()
+        {
+            if (!m_IsActive)
+            {
+                return;
+            }
+
+            OnTriggerExited?.Invoke(m_EntityId);
+        }
+
+        public void TryInteract()
+        {
+            if (!m_IsActive)
+            {
+                return;
+            }
+
+            FieldCompiledScript compiledScript = m_Entity.ScriptDefinition.GetScript(FieldScriptType.OnInteraction);
+
+            OnInteracted?.Invoke(m_EntityId, compiledScript.ScriptId);
+        }
+
+        public void SetActive(bool active)
+        {
+            m_IsActive = active;
+        }
+    }
+}
