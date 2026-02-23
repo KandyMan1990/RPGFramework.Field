ff4bba201792b7aac432bec13e60700b2f4fe32e
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-02-23 22:55:19 +0000

    Add MathUtils

---
+++ Runtime/Utils/MathUtils.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+
+namespace RPGFramework.Field.Utils
+{
+    internal static class MathUtils
+    {
+        internal static float LerpAngle(float a, float b, float t)
+        {
+            t = math.clamp(t, 0f, 1f);
+
+            float delta = math.degrees(math.atan2(math.sin(math.radians(b - a)), math.cos(math.radians(b - a))));
+
+            return a + delta * t;
+        }
+    }
+}
